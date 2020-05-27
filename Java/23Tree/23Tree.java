4a179b4f54450a3682fedacf0ce1ab097c598a98
AshenSilverwolf <ashensilverwolf@gmail.com>
2020-05-27 02:48:20 -0400

    yes

--- Java/23Tree/23Tree.java
+++ Java/23Tree/23Tree.java
@@ -1,9 +1,16 @@
 class TwoThreeNode {
-	int data1, data2;
+	int data1, data2, data3, size;
 	TwoThreeNode left, right, center;
 
 	public TwoThreeNode() {
-		this.data1 = this.data2 = 0;
+		this.data1 = this.data2 = this.data3 = this.size = 0;
+		this.left = this.right = this.center = null;
+	}
+
+	public TwoThreeNode(int key) {
+		this.data1 = key;
+		this.data2 = this.data3 = 0;
+		this.size = 1;
 		this.left = this.right = this.center = null;
 	}
 }
@@ -17,11 +24,45 @@ class TwoThreeTree {
 
 	void insert(int key) {
 
-		TwoThreeNode nearest = searchRecursive(root, key);
-		// PICK UP HERE
+		if (this.root == null) {
+			this.root = prot1(key);
+			return;
+		}
+		if (this.root.data1 == key || this.root.data2 == key)
+			return;
+
+		insertRecursive(root, key);
 	}
 
-	void split() {
+	TwoThreeNode insertRecursive(TwoThreeNode node, int key) {
+
+		if (node == null)
+			return prot1(key);
+		if (node.data1 == key || node.data2 == key)
+			return node;
+
+		
+	}
+
+	// Inserting into null node
+	TwoThreeNode prot1(int key) {
+
+		return new TwoThreeNode(key);
+	}
+
+	// Inserting into node size = 1
+	TwoThreeNode prot2(TwoThreeNode child, int key) {
+
+		if (key < child.data1) {
+			child.data2 = child.data1;
+			child.data1 = key;
+		} else {
+			child.data2 = key;
+		}
+	}
+
+	// Inserting into full node
+	TwoThreeNode prot3(TwoThreeNode child, TwoThreeNode parent, int key) {
 
 
 	}
@@ -33,26 +74,28 @@ class TwoThreeTree {
 
 	void search(int key) {
 
-		TwoThreeNode walker = root;
-		TwoThreeNode nearest = searchRecursive(walker, key);
+		boolean found = searchRecursive(this.root, key);
 
-		if (nearest.data1 == key || nearest.data2 == key)
+		if (found)
 			System.out.println("Data " + key + " Found.");
 		else
 			System.out.println("Data " + key + " Not Found.");
 	}
 
-	TwoThreeNode searchRecursive(TwoThreeNode node, int key) {
+	boolean searchRecursive(TwoThreeNode node, int key) {
 
-		if (node == null) return null;
-		if (node.data1 == key || node.data2 == key) return node;
+		if (node == null)
+			return false;
+		else if (node.data1 == key || node.data2 == key)
+			return true;
 		
-		if (key < node.data1)
+		if (key < node.data1) {
 			return searchRecursive(node.left, key);
-		else if (node.data2 != 0 && key > node.data2)
-			return searchRecursive(node.right, key);
-		else
+		} else if (node.size == 1) {
 			return searchRecursive(node.center, key);
+		} else {
+			return searchRecursive(node.right, key);
+		}
 	}
 
 	void printInorder() {

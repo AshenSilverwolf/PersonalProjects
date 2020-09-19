93d5dd8e1b1635e217029fb48e15038a4fb962b8
AshenSilverwolf <ashensilverwolf@gmail.com>
2020-09-19 17:48:46 -0400

    Intro to Python

---
+++ Python/helloWorld.py
@@ -0,0 +1,20 @@
+print('Hello, World')
+print('Gavin "quoted" this')
+print("Gavin \"quoted\" this")
+print(10 / 10)
+
+welcome = "Hello"
+name = "Gavin"
+print(welcome + ' ' + name)
+
+one = 10
+two = 10
+print(one + two)
+
+print('Please enter your name: ')
+name = input()
+print('Welcome' + ' ' + name)
+
+print('Please enter the first number: ')
+num1 = int(input())
+num2 = int(input('Please enter the second number: '))
